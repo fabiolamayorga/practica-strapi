11b34de34460b6fbb4576aef243ff7d23b3490d2
Fabiola <fab.tames@gmail.com>
2023-02-13 11:58:08 -0600

    initial commit

---
+++ src/api/question-page/controllers/question-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * question-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::question-page.question-page');
---
+++ src/api/question-page/routes/question-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * question-page router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::question-page.question-page');
